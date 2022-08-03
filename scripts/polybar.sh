4c45cf46a79482c65c7358e6fc2421d2b9f61ccf
BoManev <bomanev00@gmail.com>
2022-08-03 18:24:23 -0400

    small fixes

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -1,9 +1,9 @@
 # Terminate already running bar instances
 polybar-msg cmd quit
 
-polybar example 
+polybar mybar
 
-
+# multi monitor setup
 # if type "xrandr" > /dev/null; then
 #     for x in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 #         MONITOR = $m polybar --reload mainbar-i3 -c ~/.config/polybar/config &
