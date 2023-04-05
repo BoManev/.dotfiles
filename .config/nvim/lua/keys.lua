825d34f30239258216c3f572f7cecf75f1a9f3da
BoManev <bomanev@pm.me>
2023-04-05 00:39:02 -0400

    move to lua dir

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -4,4 +4,3 @@ vim.keymap.set('n', 'z?', function() vim.opt.spell = true end, opts)
 
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set('n', '<leader>ss', '<cmd> source ~./config/nvim/')
