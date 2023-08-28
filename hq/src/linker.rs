5b4141e850b9fb67badb67256052807f38175e18
BoManev <bomanev@pm.me>
2023-08-28 00:34:39 -0400

    new file structure

--- hq/src/linker.rs
+++ hq/src/linker.rs
@@ -1,48 +1,30 @@
 use std::fs;
 use std::path::Path;
 
-pub fn link_with_bak(source_path: &str, target_path: &str) -> Result<(), std::io::Error> {
+pub fn file_link(source_path: &str, target_path: &str) -> Result<(), std::io::Error> {
     let source = Path::new(source_path);
     let target = Path::new(target_path);
 
+    if !source.exists() {
+        panic!("Invalid source path");
+    }
     if !target.exists() || !target.is_dir() {
         panic!("Invalid target directory");
     }
 
-    if target_path.ends_with('/') {
-        let target_file = target.join(source.file_name().unwrap());
-        if target_file.exists() {
-            if let Ok(linked) = fs::read_link(&target_file) {
-                if linked == source {
-                    println!("{source_path} is already symlinked. No changes");
-                    return Ok(());
-                }
-
-                let backup_file = source.with_extension("bak");
-                fs::copy(&target_file, &backup_file).expect(&format!(
-                    "Failed to backup {}",
-                    &source.file_name().unwrap().to_string_lossy()
-                ));
-                fs::remove_file(&target_file).expect(&format!(
-                    "Failed to remove {}",
-                    &target_file.to_string_lossy()
-                ));
+    let target_file = target.join(source.file_name().unwrap());
+    if target_file.exists() {
+        if let Ok(linked) = fs::read_link(&target_file) {
+            if linked == source {
+                println!("{source_path} is already symlinked. No changes");
+                return Ok(());
             }
         }
-        std::os::unix::fs::symlink(&source_path, &target_file)?;
-    } else {
-        let backup_dir = source.with_extension("bak");
-        fs::create_dir(&backup_dir)?;
-        for file in fs::read_dir(target_path)? {
-            let file = file?;
-            let file_name = file.file_name();
-            let backup_file = backup_dir.join(file_name);
-            fs::rename(&file.path(), &backup_file)?;
-        }
-        fs::remove_dir(target_path)?;
-        println!("remove {target_path}");
-        std::os::unix::fs::symlink(&source_path, &target_path)?;
+        let backup_file = source.with_extension("bak");
+        fs::copy(&target_file, &backup_file)?;
+        fs::remove_file(&target_file)?;
     }
+    std::os::unix::fs::symlink(&source_path, &target_file)?;
 
     Ok(())
 }
--- hq/src/main.rs
+++ hq/src/main.rs
@@ -5,44 +5,47 @@ pub use linker::*;
 
 use std::{io, path::Path};
 fn main() -> io::Result<()> {
-    let input_dir = "/home/bo/projects/.dotfiles/i3wm"; // Specify your directory here
-    let output_file = "/home/bo/projects/.dotfiles/i3config";
+    let input_dir = "/home/bo/projects/.dotfiles/i3wm/config";
+    let output_file = "/home/bo/projects/.dotfiles/i3wm/i3config";
 
     let input_dir_path = Path::new(input_dir);
     let output_file_path = Path::new(output_file);
 
     concat_builder(input_dir_path, output_file_path)?;
 
-    let source_file = "/home/bo/projects/.dotfiles/.XCompose";
+    let source_file = "/home/bo/projects/.dotfiles/x11/.XCompose";
     let target_path = "/home/bo/";
-    link_with_bak(source_file, target_path)?;
+    file_link(source_file, target_path)?;
 
-    let source_file = "/home/bo/projects/.dotfiles/.xinitrc";
-    link_with_bak(source_file, target_path)?;
+    let source_file = "/home/bo/projects/.dotfiles/x11/.xinitrc";
+    file_link(source_file, target_path)?;
 
-    let source_file = "/home/bo/projects/.dotfiles/.Xmodmap";
-    link_with_bak(source_file, target_path)?;
+    let source_file = "/home/bo/projects/.dotfiles/x11/.Xmodmap";
+    file_link(source_file, target_path)?;
 
-    let source_file = "/home/bo/projects/.dotfiles/.Xresources";
-    link_with_bak(source_file, target_path)?;
+    let source_file = "/home/bo/projects/.dotfiles/x11/.Xresources";
+    file_link(source_file, target_path)?;
 
-    let source_file = "/home/bo/projects/.dotfiles/.bashrc";
-    link_with_bak(source_file, target_path)?;
+    let source_file = "/home/bo/projects/.dotfiles/bashrc/.bashrc";
+    file_link(source_file, target_path)?;
 
     let source_file = "/home/bo/projects/.dotfiles/.gitconfig";
-    link_with_bak(source_file, target_path)?;
+    file_link(source_file, target_path)?;
 
-    let source_file = "/home/bo/projects/.dotfiles/settings.json";
+    let source_file = "/home/bo/projects/.dotfiles/vscode/settings.json";
     let target_path = "/home/bo/.config/Code/User/";
-    link_with_bak(source_file, target_path)?;
+    file_link(source_file, target_path)?;
+
+    let source_file = "/home/bo/projects/.dotfiles/vscode/keybindings.json";
+    file_link(source_file, target_path)?;
 
     let source_file = "/home/bo/projects/.dotfiles/config.fish";
     let target_path = "/home/bo/.config/fish/";
-    link_with_bak(source_file, target_path)?;
+    file_link(source_file, target_path)?;
 
-   let source_file = "/home/bo/projects/.dotfiles/nvim";
-   let target_path = "/home/bo/.config/nvim";
-   link_with_bak(source_file, target_path)?;
+    //    let source_file = "/home/bo/projects/.dotfiles/nvim";
+    //    let target_path = "/home/bo/.config/nvim";
+    //     file_link(source_file, target_path)?;
 
     Ok(())
 }
