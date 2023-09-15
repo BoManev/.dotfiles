7b1d81cdd6a388d55fa47c1da9ec764e33b45b64
BoManev <bomanev@pm.me>
2023-09-15 17:42:31 -0400

    adjust low battery check interval and add expire flag for notifactions

--- scripts/lowbat-notif.sh
+++ scripts/lowbat-notif.sh
@@ -4,7 +4,7 @@ CAPACITY=$(cat /sys/class/power_supply/BAT0/capacity)
 STATUS=$(cat /sys/class/power_supply/BAT0/status)
 
 if [ "$STATUS" = "Discharging" ] && [ "$CAPACITY" -lt 20 ]; then
-    /usr/bin/notify-send -u critical "Low Battery" "Capacity: ($CAPACITY%)\n"
+    /usr/bin/notify-send --expire-time 30000 -u critical "Low Battery" "Capacity: ($CAPACITY%)\n"
 fi
 
 if [ "$STATUS" = "Discharging" ] && [ "$CAPACITY" -lt 10 ]; then
