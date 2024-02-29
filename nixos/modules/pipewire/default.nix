6832f51b49dd0b1812e9b4815f08531a2ba98334
Bo Manev <bomanev@pm.me>
2024-02-28 23:33:20 -0500

    add pipewire and sound

---
+++ nixos/modules/pipewire/default.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  sound.enable = false;
+  hardware.pulseaudio.enable = false;
+}
