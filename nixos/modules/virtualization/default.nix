280f1a35cac8773803612fbf9fec14ba4e2eaf6e
Bo Manev <bomanev@pm.me>
2024-02-28 23:50:51 -0500

    add virtualization and docker

---
+++ nixos/modules/virtualization/default.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, user, ... }:
+
+{
+  virtualisation = {
+    docker = {
+      enable = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
+    };
+    libvirtd.enable = true;
+    virtualbox.host.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    docker-compose
+  ];
+
+  users.users.${user}.extraGroups = [ "libvirtd" "docker" ];
+  users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
+}
